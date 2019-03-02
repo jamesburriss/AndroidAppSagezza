c9f9e586d9a6af2de0bd6c855a71b06a9fc2dd97
HusainC <h.chopdawala@newcastle.ac.uk>
2019-03-02 16:10:38 +0000

    added login button functions

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,23 +18,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username = (EditText)findViewById(R.id.EmailId);
+        username = (EditText)findViewById(R.id.editTextEmail);
         password = (EditText)findViewById(R.id.editTextPassword);
         login = (Button)findViewById(R.id.btnLogin);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                login.setText("Button has been pressed");
-                login.setText("The large text has been ");
-                //validate(username.getText().toString(), password.getText().toString());
+                validate(username.getText().toString(), password.getText().toString());
             }
         });
 
     }
     private void validate(String userName, String userPassword){
         if((userName.equals("Admin")) && (userPassword.equals("1234"))){
-            Intent intent = new Intent(LoginActivity.this, GuestActivity.class);
+            Log.i("TEST", "If passed!!!");
+            Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
             startActivity(intent);
         }
     }
