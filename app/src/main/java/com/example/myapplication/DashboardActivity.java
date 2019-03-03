cedc0923d02893d7fb8f55fd2b1c0fa3d5d1139f
James Burriss <me@jamesburriss.com>
2019-03-03 17:10:08 +0000

    Login page work

--- app/src/main/java/com/example/myapplication/DashboardActivity.java
+++ app/src/main/java/com/example/myapplication/DashboardActivity.java
@@ -2,12 +2,21 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class DashboardActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
+
+
+
+
 }
