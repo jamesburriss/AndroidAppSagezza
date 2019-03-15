fe717ba1ecd5e7c7bf48e9f9b78c8e395f8b7696
Callum Errington <callum.errington@gmail.com>
2019-03-15 03:25:18 +0000

    Deleted CustomListAdapter.java as it is no longer required Refactored activity_admin.xml to activity_user_profile.xml Converted list population to use SimpleAdapter for development purposes as it is easier to work with Redesigned listview_layout.xml to represent a simple key/value pair (i.e. gender/male) Moved string constant to strings.xml for activity_user_profile.xml

--- app/src/main/java/uk/ac/ncl/team15/android/UserProfileActivity.java
+++ app/src/main/java/uk/ac/ncl/team15/android/UserProfileActivity.java
@@ -7,9 +7,12 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.SimpleAdapter;
 
-import uk.ac.ncl.team15.android.R;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 
 public class UserProfileActivity extends AppCompatActivity {
     ListView listView;
@@ -34,25 +37,31 @@ public class UserProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
-        TextView name=(TextView) findViewById(R.id.actualName);
-        name.setText(infoArray);
-        CustomListAdapter whatever = new CustomListAdapter(this,phoneNo,emailArray,dob);
-        listView = (ListView) findViewById(R.id.ListViewId);
-        listView.setAdapter(whatever);
+        setContentView(R.layout.activity_user_profile);
 
+        ListView userAttribList = findViewById(R.id.userAttributesList);
+
+        // Below from: https://stackoverflow.com/questions/8554443/custom-list-item-to-listview-android
+        // Create the item mapping
+        String[] from = new String[] { "attribName", "attribDesc" };
+        int[] to = new int[] { R.id.attribName, R.id.attribDesc};
+
+        // Add some rows
+        List<HashMap<String, Object>> fillMaps = new ArrayList<HashMap<String, Object>>();
+
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("attribName", "Gender"); // This will be shown in R.id.attribName
+        map.put("attribDesc", "male"); // And this in R.id.attribDesc
+        fillMaps.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("attribName", "Nationality");
+        map.put("attribDesc", "British");
+        fillMaps.add(map);
+
+        SimpleAdapter adapter = new SimpleAdapter(this, fillMaps, R.layout.listview_layout, from, to);
+        userAttribList.setAdapter(adapter);
     }
-
-    String [] dob={
-            "23/06"
-    };
-
-    String infoArray =
-          "Example User";
-    String[] phoneNo = {
-            "01234567890"};
-    String[] emailArray = {"a@b.com" };
-
 }
 
 
