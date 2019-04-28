80534888c3e62da69b07741448551897e878d0d1
N-Nat <n.neo@newcastle.ac.uk>
2019-04-28 01:20:47 +0100

    Guest Login button

--- app/src/main/java/uk/ac/ncl/team15/android/LoginActivity.java
+++ app/src/main/java/uk/ac/ncl/team15/android/LoginActivity.java
@@ -18,6 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText username;
     private EditText password;
     private Button login;
+    private Button guestLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         username = (EditText)findViewById(R.id.editTextEmail);
         password = (EditText)findViewById(R.id.editTextPassword);
         login = (Button)findViewById(R.id.btnLogin);
+        guestLogin = (Button) findViewById(R.id.btnGuest);
 
         login.setOnClickListener(view -> validate(username.getText().toString(), password.getText().toString()));
     }
---
+++ app/src/main/java/uk/ac/ncl/team15/android/NavigationActivity.java
@@ -0,0 +1,15 @@
+package uk.ac.ncl.team15.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.ac.ncl.team15.android.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
